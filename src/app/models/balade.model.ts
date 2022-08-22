ba160db75f9f5cec15ab10ae212520fda53014cb
Yanis <yanis.l69520@live.fr>
2022-08-22 21:51:54 +0200

    affichage des catégories avec mise en forme

---
+++ src/app/models/balade.model.ts
@@ -0,0 +1,10 @@
+export interface Balade {
+    id: number,
+    title: string,
+    description: string,
+    imgUrl: string,
+    date: Date,
+    location: string,
+    kilometers: string,
+    idCategory: number
+}
